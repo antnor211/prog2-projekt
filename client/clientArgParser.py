dfb8b31c66062e6da7af0c61ba7f41310dd16307
cmorling <caspian.morling@gmail.com>
2022-03-15 14:49:59 +0100

    Client argparser

--- client/clientArgParser.py
+++ client/clientArgParser.py
@@ -10,7 +10,7 @@ class clientArgParser(argParser):
         argParser.__init__(self)
         self.parser.add_argument('-p', metavar='Port', type=int,
                         help='TCP port number to of Lorum Ipsum', required=True)
-        self.parser.add_argument('-H', metavar='Host', type=str,
+        self.parser.add_argument('-i', metavar='Host', type=str,
                         help='Host ip to Lorum Ipsum', required=True)
         self.loadArgs()
 
