7942ac124c02a2781376643a544ebd4401a23318
Airbag65 <normananton03@gmail.com>
2022-03-15 15:50:24 +0100

    test

--- client/clientArgParser.py
+++ client/clientArgParser.py
@@ -1,13 +1,9 @@
 from utils.argParser import ArgParser
 
-<<<<<<< HEAD
 sys.path.append(os.getcwd())
 from utils.argParser import argParser
 
-class ClientArgParser(argParser):
-=======
 class ClientArgParser(ArgParser):
->>>>>>> 7fe4743bd6d476bfd7cd48ae8b146ecfca694680
     def __init__(self):
         ArgParser.__init__(self)
         self.parser.add_argument('-p', metavar='Port', type=int,
