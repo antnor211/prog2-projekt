5d1ab8662b95fb004feaf83f6c19416a5ab25ca6
cmorling <caspian.morling@gmail.com>
2022-03-22 15:36:18 +0100

    command implementetion and moved socket implementation

---
+++ server/commands.py
@@ -0,0 +1,106 @@
+import uuid
+
+class Commands():
+    def __init__(self, database):
+        self._db = database
+
+    def login(self, command):
+        if not command['body']['username'] or not command['body']['password']:
+            return({'code': '400', 'message': 'Missing Parameters'})
+
+        userId = self._db.handleQuery(
+            (command['body']['username'],), 'getUserbyUsername')
+        if len(userId) == 0:
+            return({
+                'code': '401',
+                'message': 'Username or password is incorrect'
+            })
+        password = self._db.handleQuery((userId[0][0],), 'getPassword')
+        if password[0][0] != command['body']['password']:
+            return({
+                'code': '401',
+                'message': 'Username or password is incorrect'
+            })
+        newSess = self._db.handleUpdate(
+            (str(uuid.uuid4()), userId[0][0]), 'session')
+
+        return {
+            'code': '200',
+            'session': newSess[0],
+        }
+
+    def createUser(self, command):
+        if not command['body']['username'] or not command['body']['password'] or not command['body']['publicKey']:
+            return({'code': '400', 'message': 'Missing Parameters'})
+        userId = self._db.handleQuery(
+            (command['body']['username'],), 'getUserbyUsername')
+        if len(userId) != 0:
+            return({'code': '400', 'message': 'Username Taken'})
+        mResponse = self._db.handleMutation(
+            (command['body']['username'], str(uuid.uuid4()), command['body']['password'], command['body']['publicKey'], 'foo'), 'createUser')
+        newSess = self._db.handleUpdate(
+            (str(uuid.uuid4()), mResponse[1]), 'session')
+        return {
+            'code': '200',
+            'session': newSess[0],
+        }
+
+    def deleteUser(self, command):
+        if not command['body']['username'] or not command['session']:
+            return({'code': '400', 'message': 'Missing Parameters'})
+        userId = self._db.handleQuery(
+            (command['body']['username'],), 'getUserbyUsername')
+        if len(userId) == 0:
+            return({'code': '400', 'message': 'Username Does not match any records'})
+        session = self._db.handleQuery(
+            (userId[0][0],), 'getCurrentSession')
+        if session[0][0] != command['session']:
+            return({'code': '401', 'message': 'No session established'})
+        self._db.handleMutation(
+            (userId[0][0],), 'deleteUser')
+
+        return {
+            'code': '200',
+        }
+
+    def newPassword(self, command):
+        if not command['body']['username'] or not command['session'] or not command['body']['newPassword']:
+            return({'code': '400', 'message': 'Missing Parameters'})
+        userId = self._db.handleQuery(
+            (command['body']['username'],), 'getUserbyUsername')
+        if len(userId) == 0:
+            return({'code': '400', 'message': 'Username Does not match any records'})
+        session = self._db.handleQuery(
+            (userId[0][0],), 'getCurrentSession')
+        if session[0][0] != command['session']:
+            return({'code': '401', 'message': 'No session established'})
+        self._db.handleUpdate(
+            (command['body']['newPassword'], userId[0][0],), 'newPassword')
+
+        return {
+            'code': '200',
+        }
+
+
+    def logout(self, command):
+        if not command['body']['username']:
+            return({'code': '400', 'message': 'Missing Parameters'})
+        userId = self._db.handleQuery(
+            (command['body']['username'],), 'getUserbyUsername')
+        if len(userId) == 0:
+            return({'code': '400', 'message': 'Username Does not match any records'})
+        self._db.handleUpdate(
+            (str(uuid.uuid4()), userId[0][0]), 'session')
+
+    def handleCommand(self, command):
+        try:
+            return getattr(self, command['head'])(command)
+        except Exception as e:
+            print('exception reached')
+            print(e)
+            return Exception
+
+
+if __name__ == '__main__':
+    pass
+
--- server/main.py
+++ server/main.py
@@ -5,19 +5,21 @@ import json
 sys.path.append(os.getcwd())
 
 from socketServer import SocketServer
+from commands import Commands
 
 if __name__ == "__main__":
     with SocketServer("localhost", 8000) as ss:
-        while True:
-            conn, addr = ss.accept()
-            msg = conn.recv(1024)
-            response = 'foo bar' #test purposes
-            try: 
-                payload = json.loads(msg.decode('utf-8'))
-                print('payload', payload)
-                print('response', response)
-                conn.send(json.dumps(response).encode())
-            except:
-                pass
-            finally:
-                conn.close()
+        with Commands(None) as commands:
+            while True:
+                msg = ss.data
+                if msg:
+                    try:
+                        payload = json.loads(msg.decode('utf-8'))
+                        response = commands.handleCommand(payload)
+                        print('payload', payload)
+                        print('response', response)
+                        ss.sock.conn.send(json.dumps(response).encode())
+                    except:
+                        pass
+                    finally:
+                        ss.sock.conn.close()
