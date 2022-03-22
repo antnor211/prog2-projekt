8b3dabe6260ae6ddb4726828982f8ff24f26f2d1
Airbag65 <normananton03@gmail.com>
2022-03-22 14:47:53 +0100

    migrage.py done

--- database/migrate.py
+++ database/migrate.py
@@ -1,10 +1,12 @@
 import sqlite3
 
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+
 sql_file = open("migrate.sql", "r")
 sql_data = sql_file.readlines()
 sql_file.close()
 
-print(sql_data)
 
 sqlite_query = ""
 
@@ -17,12 +19,12 @@ for line in sql_data:
     if sqlite_query.endswith(";"):
         exec_sql = sqlite_query.replace("    ", "")
         print(exec_sql)
-        con = sqlite3.connect("database.db")
-        cur = con.cursor()
         cur.execute(exec_sql)
-        con.commit()
-        con.close()
         sqlite_query = ""
         print("END")
     else:
         continue
+
+
+con.commit()
+con.close()
