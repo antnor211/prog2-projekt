d19c8309b74bb2a3242d501d89a3c28e544c3762
cmorling <caspian.morling@gmail.com>
2022-03-22 15:35:17 +0100

    socket OOP

--- server/socketServer.py
+++ server/socketServer.py
@@ -1,30 +1,34 @@
+from contextlib import nullcontext
 import socket
-import sys
-import os
+
 
 from utils.socketParent import SocketParent
 
 class SocketServer(SocketParent):
     def __init__(self, host, port):
+        self.data = 'None'
         SocketParent.__init__(self, host, port)
 
     def __enter__(self):
+        
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((self._host, self._port))
         print('[*] Server running on {}'.format(self._port))
         s.listen(10)
-        connector, address = s.accept()
-        with connector:
-            print(f"Connected to {address}")
-            while True:
-                data = connector.recv(1024)
-                if not data:
-                    break
-                connector.sendall(data)
-        self._sock = s
+        self.connector, self.address = s.accept()
 
-        return self._sock
+        with self.connector:
+            print(f"Connected to {self.address}")
+            while True:
+                self.data = self.connector.recv(1024)
+                if not self.data:
+                    break
+                self.connector.sendall(self.data)
+            self.data = None
+        self.sock = s
+
+        return self
 
     def __exit__(self, *exc_info):
         SocketParent.__exit__(self, exc_info)
--- utils/socketParent.py
+++ utils/socketParent.py
@@ -3,7 +3,7 @@ class SocketParent:
         self._host = host
         self._port = port
     def __exit__(self, exc_info):
-        self._sock.close()
+        self.sock.close()
         if exc_info[0]:
             import traceback
             traceback.print_exception(*exc_info)
