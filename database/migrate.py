b490c527d9dd95a6ad8ecccb99e1e82d612ff906
Airbag65 <normananton03@gmail.com>
2022-03-22 15:24:39 +0100

    migrate.py oop

--- database/migrate.py
+++ database/migrate.py
@@ -1,30 +1,40 @@
+from hashlib import new
 import sqlite3
 
-con = sqlite3.connect("database.db")
-cur = con.cursor()
+class Migrate:
+    def __init__(self, path_database, path_sql_file):
+        self.path_database = path_database
+        self.con = sqlite3.connect(self.path_database)
+        self.cur = self.con.cursor()
+        self.path_sql_file = path_sql_file
+        self.sql_file = None
+        self.sql_data = None
+        self.sql_query = ""
+    
+    def _open_sql(self): 
+        self.sql_file = open(self.path_sql_file, "r")
+        self.sql_data = self.sql_file.readlines()
+        self.sql_file.close()
+    
+    def migrate_data(self):
+        self._open_sql()
+        for line in self.sql_data:
+            new_line = line.replace("\n", "")
+            if new_line == "":
+                continue
+            self.sql_query += new_line
+            if self.sql_query.endswith(";"):
+                exec_sql = self.sql_query.replace("    ", "")
+                print(exec_sql)
+                self.cur.execute(exec_sql)
+                self.sql_query = ""
+                print("END of quary line")
+            else:
+                continue
+        self.con.commit()
+        self.con.close()
 
-sql_file = open("migrate.sql", "r")
-sql_data = sql_file.readlines()
-sql_file.close()
 
 
-sqlite_query = ""
-
-for line in sql_data:
-    new_line = line.replace("\n", "")
-
-    if new_line == "":
-        continue
-    sqlite_query += new_line
-    if sqlite_query.endswith(";"):
-        exec_sql = sqlite_query.replace("    ", "")
-        print(exec_sql)
-        cur.execute(exec_sql)
-        sqlite_query = ""
-        print("END")
-    else:
-        continue
-
-
-con.commit()
-con.close()
+migrate = Migrate("database.db", "migrate.sql")
+migrate.migrate_data()
